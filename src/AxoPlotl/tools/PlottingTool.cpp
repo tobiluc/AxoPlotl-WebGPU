da1434fb7dc7b6c27d5d51fe711c93b0e0b7be31
Tobias Kohler <tobias.kohler@unibe.ch>
2026-02-24 23:55:43 +0100

    wip

--- src/AxoPlotl/tools/PlottingTool.cpp
+++ src/AxoPlotl/tools/PlottingTool.cpp
@@ -10,30 +10,54 @@ void PlottingTool::render_ui(Application& app)
 {
     if (!ImGui::CollapsingHeader("Plotting")) {return;}
 
-    ImGui::InputText("##x", input_buffer_, sizeof(input_buffer_));
+    ImGui::InputText("x(u,v)", x_input_.data(), 1024);
+    ImGui::InputText("y(u.v)", y_input_.data(), 1024);
+    ImGui::InputText("z(u,v)", z_input_.data(), 1024);
+    ImGui::InputFloat2("u", &u_range_[0]);
+    ImGui::InputFloat2("v", &v_range_[0]);
+    ImGui::InputInt("Resolution", &resolution_);
 
     if (ImGui::Button("Submit")) {
         // Parse Input Text
         ibex::Functions funcs = ibex::common_functions();
         ibex::Variables vars = ibex::common_variables();
-        auto rpn = ibex::generate_postfix(ibex::tokenize(input_buffer_));
-        if (!rpn.empty())
+        auto rpn_x = ibex::generate_postfix(ibex::tokenize(x_input_.c_str()));
+        auto rpn_y = ibex::generate_postfix(ibex::tokenize(y_input_.c_str()));
+        auto rpn_z = ibex::generate_postfix(ibex::tokenize(z_input_.c_str()));
+        if (!rpn_x.empty() && !rpn_y.empty() && !rpn_z.empty())
         {
-            auto func = [&](const float& v) {
-                vars["x"] = v;
-                return ibex::eval_postfix(rpn, vars, funcs);
+            auto func = [&](const float& u, const float& v) -> Vec3f {
+                vars["u"] = u;
+                vars["v"] = v;
+                return Vec3f{
+                    ibex::eval_postfix(rpn_x, vars, funcs),
+                    ibex::eval_postfix(rpn_y, vars, funcs),
+                    ibex::eval_postfix(rpn_z, vars, funcs)
+                };
             };
 
-            for (int i = 0; i < samples_.size(); ++i) {
-                float x = (float)i/(float)samples_.size();
-                float y = func(x);
-                if (!std::isnan(y)) {
-                    samples_[i] = y;
+            VolumeMesh mesh;
+            for (int i = 0; i <= resolution_; ++i) {
+                float u = u_range_.x + ((float)i/resolution_)*(u_range_.y-u_range_.x);
+                for (int j = 0; j <= resolution_; ++j) {
+                    float v = v_range_.x + ((float)j/resolution_)*(v_range_.y-v_range_.x);
+                    auto p = func(u,v);
+                    mesh.add_vertex(OVM::Vec3f(p[0],p[1],p[2]));
                 }
             }
+            for (int i = 0; i < resolution_; ++i) {
+                for (int j = 0; j < resolution_; ++j) {
+                    OVM::VH vh0(i*(resolution_+1)+j);
+                    OVM::VH vh1(i*(resolution_+1)+j+1);
+                    OVM::VH vh2((i+1)*(resolution_+1)+j+1);
+                    OVM::VH vh3((i+1)*(resolution_+1)+j);
+                    mesh.add_face({vh0,vh1,vh2,vh3});
+                }
+            }
+            app.scene().add_object<VolumeMeshObject>(mesh);
         }
     }
-    ImGui::PlotLines("##y", samples_.data(), samples_.size());
+    // ImGui::PlotLines("##y", samples_.data(), samples_.size());
 }
 
 }
--- src/AxoPlotl/tools/PlottingTool.hpp
+++ src/AxoPlotl/tools/PlottingTool.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "AxoPlotl/AxoPlotl_fwd.hpp"
+#include "AxoPlotl/typedefs/glm.hpp"
 #include <array>
 #include <cstring>
+#include <string>
 
 namespace AxoPlotl
 {
@@ -10,16 +12,23 @@ class PlottingTool
 {
 public:
     PlottingTool() {
-        strncpy(input_buffer_, "", sizeof(input_buffer_)-1);
-        input_buffer_[sizeof(input_buffer_)-1] = '\0';
-        for (int i = 0; i < sizeof(samples_); ++i) {samples_[i] = 0;}
+        // strncpy(u_input_buffer_, "", sizeof(u_input_buffer_)-1);
+        // input_buffer_[sizeof(input_buffer_)-1] = '\0';
+        // for (int i = 0; i < sizeof(samples_); ++i) {samples_[i] = 0;}
+
     }
 
     void render_ui(Application& app);
 
 private:
-    char input_buffer_[1024];
-    std::array<float,100> samples_;
+    std::string x_input_;
+    std::string y_input_;
+    std::string z_input_;
+    // char u_input_buffer_[1024];
+    // char v_input_buffer_[1024];
+    Vec2f u_range_ = {0,1};
+    Vec2f v_range_ = {0,1};
+    int resolution_ = 10;
 };
 
 }
